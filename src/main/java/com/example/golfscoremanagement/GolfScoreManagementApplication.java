f273c061d6ae596e9146fe555c4cb38822f70a2f
shimabuku <24shimabuku_ginji@axiz.co.jp>
2024-06-04 15:44:37 +0900

    初期状態

---
+++ src/main/java/com/example/golfscoremanagement/GolfScoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.golfscoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfScoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfScoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/golfscoremanagement/GolfScoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.golfscoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfScoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
